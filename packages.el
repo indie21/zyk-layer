2abc26349645b529bd80c7ad80d2762bd242f7d0
zhuoyikang <zhuoyikang@gmail.com>
2016-04-16 19:03:59 +0800

    first commit

--- packages.el
+++ packages.el
@@ -59,4 +59,106 @@ Each entry is either:
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
 
+
+;;----------------------------------------------------------------------------
+;; 归档已完成的任务
+;;----------------------------------------------------------------------------
+
+
+(defun my-org-archive-done-tasks ()
+  (interactive)
+  (org-map-entries 'org-archive-subtree "/DONE" 'file))
+
+;;显示不够的内容。自动换行.
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+
+
+;;----------------------------------------------------------------------------
+;; 让你的上下左右键可以用来切换窗口
+;;----------------------------------------------------------------------------
+
+(when (fboundp 'winner-mode)
+  (winner-mode 1))
+(windmove-default-keybindings)
+(global-set-key (kbd "<f7> ") 'winner-undo)
+
+
+;;----------------------------------------------------------------------------
+;; Grep
+;;----------------------------------------------------------------------------
+
+(require 'grep)
+(grep-apply-setting 'grep-command  "grep -nr ")
+
+
+(setq org-agenda-files (list "~/source/gtd" ))
+
+
+
+;;org-mode 和winner模式冲突.
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
+
+;;----------------------------------------------------------------------------
+;; 习惯按键设置
+;;----------------------------------------------------------------------------
+
+(global-set-key (kbd "C-c C-b")   'ibuffer)
+(global-set-key (kbd "C-c C-c")   'comment-region)
+(global-set-key (kbd "C-c C-u")   'uncomment-region)
+(global-set-key (kbd "C-q") 'backward-kill-word)
+(global-set-key (kbd "C-c C-f") 'goto-line)
+(global-set-key (kbd "C-c C-m") 'execute-extended-command)
+;;(global-set-key (kbd "C-c C-m") 'helm-M-x)
+
+(global-set-key (kbd "M-[") 'previous-buffer)
+(global-set-key (kbd "M-]") 'next-buffer)
+
+(global-set-key (kbd "s-<left>") 'ns-prev-frame)
+(global-set-key (kbd "s-<right>") 'ns-next-frame)
+
+(global-set-key (kbd "M-|") 'indent-region)
+(global-set-key (kbd "M-h") 'mark-paragraph)
+
+;; 多个windows窗口切换.
+(global-set-key (kbd "s-[") 'ns-prev-frame)
+(global-set-key (kbd "s-]") 'ns-next-frame)
+
+(global-set-key (kbd "C-x C-k") 'kill-region)
+(global-set-key (kbd "C-c C-f") 'goto-line)
+
+
+;;(global-set-key (kbd "C-<up>") 'backward-paragraph)
+;;(global-set-key (kbd "C-<down>") 'forward-paragraph)
+
+(global-set-key (kbd "C-c v") 'ff-find-other-file)
+
+(desktop-save-mode)
+;;(desktop-revert)
+;;光标显示成一条线。
+;;(setq-default cursor-type 'bar)
+
+
+;;----------------------------------------------------------------------------
+;; 习惯按键设置
+;;----------------------------------------------------------------------------
+
+;; 打开大文件
+(defun large-file-hook ()
+  "If a file is over a given size, make the buffer read only."
+  (when (> (buffer-size) (* 2 1024 1024))
+    (setq buffer-read-only t)
+    (buffer-disable-undo)
+    (buffer-disable-undo)
+    (linum-mode 0)
+    (font-lock-mode 0)
+    (fundamental-mode)))
+
+
+(add-hook 'find-file-hooks 'large-file-hook)
+
+
 ;;; packages.el ends here
